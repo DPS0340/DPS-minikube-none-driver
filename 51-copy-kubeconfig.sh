9bb7593b9f9f382befb0aeba90a573aa0a762fec
Jiho Lee <optional.int@kakao.com>
2024-05-15 18:28:27 +0900

    feat: Remove existing directories before copy configs

--- 51-copy-kubeconfig.sh
+++ 51-copy-kubeconfig.sh
@@ -6,6 +6,8 @@ PS4="\n\033[1;33m>>\033[0m "; set -x
 LOCATION=$(realpath "$0")
 DIR=$(dirname "$LOCATION")
 
+rm -rf ~/.minikube ~/.kube/config
+
 sudo cp -r /root/.minikube ~/.minikube
 
 sudo chown -R $USER ~/.minikube
